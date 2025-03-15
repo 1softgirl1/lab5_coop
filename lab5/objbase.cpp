709726b3ef75e3ee369ca516d35fb3838cc8568c
1softgirl1 <karavaeva.lidaa@yandex.ru>
2025-03-15 13:49:09 +0700

    Add files via upload

--- lab5/objbase.cpp
+++ lab5/objbase.cpp
@@ -2,6 +2,7 @@
 #include <unknwn.h>
 #include "objbase.h"
 #include <cassert>
+#include "../Desktop/lab5/objbase.h"
 
 using namespace std;
 
@@ -73,6 +74,7 @@ private:
     long m_cRef;
 };
 
+
 // Функция создания компонента
 IUnknown* CreateInstance() {
     IUnknown* pI = static_cast<IX*>(new CA);
@@ -80,31 +82,187 @@ IUnknown* CreateInstance() {
     return pI;
 }
 
+BOOL SameComponents(IX* pIX, IY* pIY)
+{
+    IUnknown* pI1 = NULL;
+    IUnknown* pI2 = NULL;
+
+    // Получить указатель на IUnknown через pIX
+    pIX->QueryInterface(IID_IUnknown1, (void**)&pI1);
+
+    // Получить указатель на IUnknown через pIY
+    pIY->QueryInterface(IID_IUnknown1, (void**)&pI2);
+
+    // Сравнить полученные указатели
+    return pI1 == pI2;
+}
+
+void f(IX* pIX)
+{
+    IX* pIX2 = NULL;
+
+    // Запросить IX через IX
+    HRESULT hr = pIX->QueryInterface(IID_IX, (void**)&pIX2);
+
+    assert(SUCCEEDED(hr)); // Запрос должен быть успешным
+}
+
+void f2(IX* pIX)
+{
+    HRESULT hr;
+
+    IX* pIX2 = NULL;
+    IY* pIY = NULL;
+
+    // Получить IY через IX
+    hr = pIX->QueryInterface(IID_IY, (void**)&pIY);
+
+    if (SUCCEEDED(hr))
+    {
+        // Получить IX через IY
+        hr = pIY->QueryInterface(IID_IX, (void**)&pIX2);
+
+        // QueryInterface должна отработать успешно
+        assert(SUCCEEDED(hr));
+    }
+}
+
+void f3(IX* pIX)
+{
+    HRESULT hr;
+    IY* pIY = NULL;
+
+    // Запросить IY у IX
+    hr = pIX->QueryInterface(IID_IY, (void**)&pIY);
+
+    if (SUCCEEDED(hr))
+    {
+        IZ* pIZ = NULL;
+
+        // Запросить IZ и IY
+        hr = pIY->QueryInterface(IID_IZ, (void**)&pIZ);
+
+        if (SUCCEEDED(hr))
+        {
+            // Запросить IZ у IX
+            hr = pIX->QueryInterface(IID_IZ, (void**)&pIZ);
+            assert(SUCCEEDED(hr)); // Это должно работать
+        }
+    }
+}
+
+
+
+
 
 // Клиент
 int main() {
 
     setlocale(LC_ALL, "Russian");
-    // Создать компонент
-    IUnknown* pIUnknown1 = CreateInstance();
+    cout << "Hello, world!" << endl;
 
-    IX* pIX = NULL;  // Создать pIX
-    HRESULT hr = pIUnknown1->QueryInterface(IID_IX, (void**)&pIX);
+    HRESULT hr;
 
-    pIUnknown1->Release();  // Завершить работу с IUnknown
+    cout << "Client: get pointer to IUnknown" << endl;
+    IUnknown* pIUnkonwn = CreateInstance();
 
-    if (SUCCEEDED(hr))
-    {
-        pIX->Fx();  // Использовать интерфейс IX
-
-        IX* pIX2 = pIX; // Создать копию pIX
-        pIX2->AddRef();  // Увеличить счетчик ссылок
-        pIX2->Fx();     // Что-то делать при помощи pIX2
-        pIX2->Release(); // Завершить работу с pIX2
-
-        pIX->Release();  // Завершить работу с pIX
+    cout << "\nClient: get pointer to IX" << endl;
+    IX* pIX = NULL;
+    hr = pIUnkonwn->QueryInterface(IID_IX, (void**)&pIX);
+    if (SUCCEEDED(hr)) {
+        cout << "Client: IX received successfully" << endl;
+        pIX->Fx(); // использовать интерфейс IX
+        pIX->Release();
     }
 
+    cout << "\nClient: get pointer to IY" << endl;
+    IY* pIY = NULL;
+    hr = pIUnkonwn->QueryInterface(IID_IY, (void**)&pIY);
+    if (SUCCEEDED(hr)) {
+        cout << "Client: IY received successfully" << endl;
+        pIY->Fy(); // использовать интерфейс IY
+        pIY->Release();
+    };
+
+    cout << "\nClient: Get unsupported interface" << endl;
+    IZ* pIZ = NULL;
+    hr = pIUnkonwn->QueryInterface(IID_IZ, (void**)&pIZ);
+    if (SUCCEEDED(hr)) {
+        cout << "Client: interface IZ get succesfully" << endl;
+        pIZ->Release();
+    }
+    else {
+        cout << "Client: Can not get interface IZ" << endl;
+    }  
+    cout << "\nКлиент: Получить указатель на IY через IX" << endl;
+    IY* pIYfromIX = NULL;
+    hr = pIX->QueryInterface(IID_IY, (void**)&pIYfromIX);
+    if (SUCCEEDED(hr)) {
+        cout << "Клиент: указатель на IY получен успешно" << endl;
+        pIYfromIX->Fy();
+        pIYfromIX->Release();
+    }
+    else {
+        cout << "Клиент: Невозможно получить указатель на IY через IX" << endl;
+    };
+
+    cout << "\nКлиент: Получить указатель на IUnknown через IY" << endl;
+    IUnknown* pIUnknownFromIY = NULL;
+    hr = pIY->QueryInterface(IID_IUnknown1, (void**)&pIUnknownFromIY);
+    if (SUCCEEDED(hr)) {
+        cout << "Равны два указателя?" << endl;
+        if (pIUnknownFromIY == pIUnkonwn) {
+            cout << "ДА" << endl;
+            pIUnknownFromIY->Release();
+        }
+        else {
+            cout << "HET" << endl;
+        }
+    };
+  
+
+    // Тест функции f
+    cout << "\nТест f()" << endl;
+    try {
+        f(pIX);
+        cout << "f() выполнена успешно" << endl;
+    }
+    catch (...) {
+        cout << "Ошибка при выполнении f()" << endl;
+    }
+
+    // Тест функции f2
+    cout << "\nТест f2()" << endl;
+    try {
+        f2(pIX);
+        cout << "f2() выполнена успешно" << endl;
+    }
+    catch (...) {
+        cout << "Ошибка при выполнении f2()" << endl;
+    }
+
+    // Тест функции f3
+    cout << "\nТест f3()" << endl;
+    try {
+        f3(pIX);
+        cout << "f3() выполнена успешно" << endl;
+    }
+    catch (...) {
+        cout << "Ошибка при выполнении f3()" << endl;
+    }
+
+    // Тест функции SameComponents
+    cout << "\nТест SameComponents()" << endl;
+    bool result = SameComponents(pIX, pIY);
+    cout << "Результат сравнения компонентов: " << (result ? "указатели совпадают" : "указатели различаются") << endl;
+
+    // Добавьте очистку памяти после тестов
+    if (pIX != NULL) pIX->Release();
+    if (pIY != NULL) pIY->Release();
+
+    // Удалить компонент
+    delete pIUnkonwn;
+      
     return 0;
 
 }
